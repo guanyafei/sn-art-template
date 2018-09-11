52969d0f7f04e892b9673291a2fa8c1a30536803
jcrom <jcrom@163.com>
2018-09-11 15:26:51 +0800

    添加handlebars helper 支持

--- src/compile/adapter/include.js
+++ src/compile/adapter/include.js
@@ -6,14 +6,35 @@
  * @param   {Object}    options
  * @return  {string}
  */
-const include = (filename, data, blocks, options) => {
-    const compile = require('../index');
-    options = options.$extend({
-        filename: options.resolveFilename(filename, options),
-        bail: true,
-        source: null
-    });
-    return compile(options)(data, blocks);
+
+const handlebars = require('handlebars');
+const path = require('path');
+const fs = require('fs');
+
+var include = function include(filename, data, blocks, options) {
+    var ext = path.extname(filename).toLocaleLowerCase();
+    if (ext === ".hbs") {
+
+        var filename = options.resolveFilename(filename, options);
+        //TODO: 判断文件存在， 判断handlebars的入参
+        var tpl = fs.readFileSync(filename, 'utf8');
+
+        let localHelpers = options.hbsHelper;
+        if (localHelpers && typeof(localHelpers)=='object'){
+          for (const key of Object.keys(localHelpers)) {
+            handlebars.registerHelper(key, localHelpers[key]);
+          }
+        }
+        return handlebars.compile(tpl)(data);
+    } else {
+        var compile = require('../index');
+        options = options.$extend({
+            filename: options.resolveFilename(filename, options),
+            bail: true,
+            source: null
+        });
+        return compile(options)(data, blocks);
+    }
 };
 
 module.exports = include;
